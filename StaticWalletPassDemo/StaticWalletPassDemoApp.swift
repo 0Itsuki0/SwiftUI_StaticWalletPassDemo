5d29dcd1bb0c15125e1172de38b400ccf4b4c7d3
itsuki <itsuki.enjyoy@gmail.com>
2025-11-18 10:32:09 +0900

    Initial Commit

---
+++ StaticWalletPassDemo/StaticWalletPassDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticWalletPassDemoApp.swift
+//  StaticWalletPassDemo
+//
+//  Created by Itsuki on 2025/11/18.
+//
+
+import SwiftUI
+
+@main
+struct StaticWalletPassDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
